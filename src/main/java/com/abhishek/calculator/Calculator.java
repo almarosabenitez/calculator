d1c0775281fba68c5bfede056a1ffe7763b9edac
Abhishek Kuri <a.kuri@tcs.com>
2018-05-08 21:57:20 +0530

    made correction for static code analysis

--- src/main/java/com/abhishek/calculator/Calculator.java
+++ src/main/java/com/abhishek/calculator/Calculator.java
@@ -1,6 +1,8 @@
 package com.abhishek.calculator;
 import org.springframework.stereotype.Service;
-
+/**
+ * Main Business logic.
+ */
 @Service
 public class Calculator {
      int sum(int a, int b) {
